41bd72bf4dc2dcd89c4094ca7d6e59dc88a1e3e9
memintan <memintan@gmail.com>
2020-10-17 13:47:04 -0500

    Spring Container

--- Spring-01-SpringContainer/src/main/java/services/PartTimeMentor.java
+++ Spring-01-SpringContainer/src/main/java/services/PartTimeMentor.java
@@ -5,6 +5,6 @@ import interfaces.Mentor;
 public class PartTimeMentor implements Mentor {
     @Override
     public void createAccount() {
-        System.out.println("Part Time Mentor is created");
+        System.out.println("Part Time Mentor Account is created");
     }
 }
