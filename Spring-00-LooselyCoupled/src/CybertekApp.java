a26030ed9e29c33cfc398315c85dd50e10a279e0
memintan <memintan@gmail.com>
2020-10-19 19:21:16 -0500

    Loosely Coupling

--- Spring-00-LooselyCoupled/src/CybertekApp.java
+++ Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -1,12 +1,20 @@
 import implementation.Mentor;
 import service.FullTimeMentor;
 import service.MentorAccount;
+import service.PartTimeMentor;
+
+import java.util.Arrays;
 
 public class CybertekApp {
     public static void main(String[] args) {
 
-        FullTimeMentor fullTime = new FullTimeMentor();
-        MentorAccount mentor = new MentorAccount(fullTime);
+        //FullTimeMentor fullTime = new FullTimeMentor();
+        PartTimeMentor partTime = new PartTimeMentor();
+
+        MentorAccount mentor = new MentorAccount(partTime);
         mentor.manageAccount();
+
+
+
     }
 }
