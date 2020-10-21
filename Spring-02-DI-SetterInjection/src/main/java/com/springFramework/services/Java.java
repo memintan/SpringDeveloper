6c74821774603f02843e602a7865a90b8ea6732e
memintan <memintan@gmail.com>
2020-10-21 16:55:08 -0500

    DI-Setter Injection

--- Spring-02-DI-SetterInjection/src/main/java/com/springFramework/services/Java.java
+++ Spring-02-DI-SetterInjection/src/main/java/com/springFramework/services/Java.java
@@ -2,17 +2,23 @@ package com.springFramework.services;
 
 import com.springFramework.interfaces.Course;
 import com.springFramework.interfaces.ExtraSessions;
+import lombok.Data;
 
+@Data
 public class Java implements Course {
 
-    ExtraSessions extraSessions;
+    private ExtraSessions extraSessions;
 
-    public Java(ExtraSessions extraSessions) {
-        this.extraSessions=extraSessions;
-    }
+
+
+
 
     @Override
     public void getTeachingHours() {
         System.out.println("Weekly Teaching Hours : " + (20 + extraSessions.getHours())) ;
+    }
+
+
+    public void setExtraSessions(OfficeHours extraSessions) {
     }
 }
