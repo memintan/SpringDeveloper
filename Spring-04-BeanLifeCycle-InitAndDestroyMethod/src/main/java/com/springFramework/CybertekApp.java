b0069f51b1234d1ef53954ce9497ed72ccfc5b00
memintan <memintan@gmail.com>
2020-10-22 01:23:39 -0500

    Bean LifeCycle

---
+++ Spring-04-BeanLifeCycle-InitAndDestroyMethod/src/main/java/com/springFramework/CybertekApp.java
@@ -0,0 +1,16 @@
+package com.springFramework;
+
+import com.springFramework.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CybertekApp {
+    public static void main(String[] args) {
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+        Course course = container.getBean("java", Course.class);
+        course.getTeachingHours();
+
+        ((ClassPathXmlApplicationContext)container).close();
+
+    }
+}
