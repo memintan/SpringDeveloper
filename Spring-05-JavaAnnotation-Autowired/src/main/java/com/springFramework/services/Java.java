ebaf82827e62a439007594d42b094057d79c3305
memintan <memintan@gmail.com>
2020-10-26 14:20:09 -0500

    Java Annotation Autowired

--- Spring-05-JavaAnnotation-Autowired/src/main/java/com/springFramework/services/Java.java
+++ Spring-05-JavaAnnotation-Autowired/src/main/java/com/springFramework/services/Java.java
@@ -1,14 +1,36 @@
 package com.springFramework.services;
 
 import com.springFramework.interfaces.Course;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+//@AllArgsConstructor
 public class Java implements Course {
+    @Autowired
     private OfficeHours officeHours;
+/*
+    //Constructor injection
+    @Autowired
+    public Java(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
+
+ */
+
+
+    /*
+    //Setter injection
+    @Autowired
+    public void setOfficeHours(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
+
+     */
 
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours: "+30+officeHours.getHours());
+        System.out.println("Weekly teaching hours: "+(30+officeHours.getHours()));
     }
 }
