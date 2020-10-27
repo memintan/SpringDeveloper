bba4fac90d547cecde2fbd3a227254abb9462ab2
memintan <memintan@gmail.com>
2020-10-27 00:16:13 -0500

    Java Code Configuration

--- Spring-06-JavaCode-Configuration/src/main/java/com/springFramework/services/Java.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/springFramework/services/Java.java
@@ -3,6 +3,7 @@ package com.springFramework.services;
 import com.springFramework.interfaces.Course;
 import com.springFramework.interfaces.ExtraSessions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
@@ -20,5 +21,21 @@ public class Java implements Course {
     @Override
     public void getTeachingHours() {
         System.out.println("Weekly teaching hours: "+(30+extraSessions.getHours()));
+    }
+
+    @Value("JD1")
+    private String batch;
+    @Value("${instructor}")
+    private String instructor;
+    @Value("${days}")
+    private String[] days;
+
+    @Override
+    public String toString() {
+        return "Java{" +
+                "extraSessions=" + extraSessions +
+                ", batch='" + batch + '\'' +
+                ", instructor='" + instructor + '\'' +
+                '}';
     }
 }
