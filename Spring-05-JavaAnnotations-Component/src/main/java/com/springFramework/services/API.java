e2d40701d4e3b373ebff03ae3db37306a02d7214
memintan <memintan@gmail.com>
2020-11-02 16:10:39 -0600

    Spring Boot Demo

--- Spring-05-JavaAnnotations-Component/src/main/java/com/springFramework/services/API.java
+++ Spring-05-JavaAnnotations-Component/src/main/java/com/springFramework/services/API.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 public class API implements Course {
     @Override
     public void getTeachingHours() {
+
         System.out.println("Weekly teaching hours: 7");
     }
 }
