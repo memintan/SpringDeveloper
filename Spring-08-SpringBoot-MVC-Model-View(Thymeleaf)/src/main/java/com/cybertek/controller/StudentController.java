36348d1ca6079ed5da9fe5a3a3012b866cdf2b2d
memintan <memintan@gmail.com>
2020-11-16 20:12:42 -0600

    MVC - model - view

--- Spring-08-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/cybertek/controller/StudentController.java
+++ Spring-08-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/cybertek/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.cybertek.controller;
 
+import com.cybertek.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +17,7 @@ import java.util.Random;
 public class StudentController {
 
     @GetMapping("/welcome")
-    public String homePage(Model model){
+    public String homePage(Model model) {
 
         model.addAttribute("name", "Infrastructure");
         model.addAttribute("course", "MVC");
@@ -38,7 +39,16 @@ public class StudentController {
         LocalDate birthday = LocalDate.now().minusYears(37);
         model.addAttribute("birthday", birthday);
 
+        Student student = new Student(1, "Mike", "Smith");
+        model.addAttribute("student", student);
 
         return "student/welcome";
     }
+
+    @GetMapping("/register")
+    public String homePage2() {
+        return "student/register";
+    }
+
+
 }
