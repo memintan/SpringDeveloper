b7233a0ad8e6f593d6fa17e9446014c0d7d426db
memintan <memintan@gmail.com>
2020-11-18 18:24:09 -0600

    SpringBoot - MVC - Form

---
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/springboot/model/Employee.java
@@ -0,0 +1,23 @@
+package com.springboot.model;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Employee {
+    private String firstName;
+    private String lastname;
+    private String birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String xipCode;
+
+
+}
