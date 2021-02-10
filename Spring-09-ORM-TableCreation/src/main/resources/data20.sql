3156defdb678067d8458116da4c158e20d2deccc
memintan <memintan@gmail.com>
2021-02-10 12:42:40 -0600

    ORM - Table Creation

--- Spring-09-ORM-TableCreation/src/main/resources/data20.sql
+++ Spring-09-ORM-TableCreation/src/main/resources/data20.sql
@@ -1,4 +1,4 @@
 INSERT INTO students(student_first_name,student_last_name,student_email-addres) VALUES('Mike','Smith','mike@cybertek.com');
 INSERT INTO students(student_first_name,student_last_name,student_email-addres) VALUES('Adam','Shaun','adam@cybertek.com');
-INSERT INTO students(student_first_name,student_last_name,student_email-addres) VALUES('Tom','Hnaks','tom@cybertek.com');
+INSERT INTO students(student_first_name,student_last_name,student_email-addres) VALUES('Tom','Hanks','tom@cybertek.com');
 INSERT INTO students(student_first_name,student_last_name,student_email-addres) VALUES('Brad','Kelly','brad@cybertek.com');
