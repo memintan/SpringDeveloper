dece1a90aea5dcf4d12103b84317e311a212a2c7
memintan <memintan@gmail.com>
2021-02-10 00:11:32 -0600

    ORM - Table Creation

---
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
@@ -0,0 +1,16 @@
+package com.cybertek.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    private Long id;
+
+    private String firstName; //first_name
+    private String LastName;
+    private String email;
+
+}
