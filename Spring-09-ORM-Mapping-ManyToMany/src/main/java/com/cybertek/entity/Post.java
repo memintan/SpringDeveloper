000f32e77ae4c97a14b00722c8935761a8e0f5d9
memintan <memintan@gmail.com>
2021-02-10 22:52:34 -0600

    ORM - Mapping - OneToMany

--- Spring-09-ORM-Mapping-ManyToMany/src/main/java/com/cybertek/entity/Post.java
+++ Spring-09-ORM-Mapping-ManyToMany/src/main/java/com/cybertek/entity/Post.java
@@ -1,13 +1,13 @@
 package com.cybertek.entity;
 
+import jdk.jfr.MemoryAddress;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -21,6 +21,15 @@ public class Post {
 
     private String title;
     private String description;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "post_tag_rel",
+                joinColumns = {@JoinColumn(name = "post_id")},inverseJoinColumns = {@JoinColumn(name ="tag_id" )})
+    private Set<Tag> tags = new HashSet<>();
+
+
+
+
 
     public Post(String title, String description) {
         this.title = title;
