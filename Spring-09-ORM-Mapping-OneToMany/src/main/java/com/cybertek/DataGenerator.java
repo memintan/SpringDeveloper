7507e1c3222055403379d7fc4c91621619f17560
memintan <memintan@gmail.com>
2021-02-10 21:47:03 -0600

    ORM - Mapping - OneToMany

---
+++ Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/DataGenerator.java
@@ -0,0 +1,18 @@
+package com.cybertek;
+
+import com.cybertek.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataGenerator implements CommandLineRunner {
+
+    @Autowired
+    PersonRepository personRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
