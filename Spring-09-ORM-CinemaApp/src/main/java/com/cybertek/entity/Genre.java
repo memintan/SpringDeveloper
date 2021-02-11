5d2076ae0a038c2e5eb09ff3e6acc79fdabac71d
memintan <memintan@gmail.com>
2021-02-11 14:03:30 -0600

    ORM - Mapping - Cinema App

--- Spring-09-ORM-CinemaApp/src/main/java/com/cybertek/entity/Genre.java
+++ Spring-09-ORM-CinemaApp/src/main/java/com/cybertek/entity/Genre.java
@@ -1,4 +1,27 @@
 package com.cybertek.entity;
 
-public class Genre {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ManyToAny;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Genre extends BaseEntity {
+
+    private String name;
+
+    @ManyToMany(mappedBy = "genreList")
+    private List<Movie> movieList = new ArrayList<>();
+
+    public Genre(String name) {
+        this.name = name;
+    }
 }
