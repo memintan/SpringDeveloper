0274296b78f392d5ef3b3c77cabf91367463e807
memintan <memintan@gmail.com>
2021-02-12 14:25:00 -0600

    ORM - Mapping - Derived Queries

---
+++ Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/DerivedQueryApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DerivedQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DerivedQueryApplication.class, args);
+	}
+
+}
