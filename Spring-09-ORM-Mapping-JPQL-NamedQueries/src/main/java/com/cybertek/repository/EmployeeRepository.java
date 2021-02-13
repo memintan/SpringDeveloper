f66574dbe0f14a37f0fc53809efb5015cbd13f15
memintan <memintan@gmail.com>
2021-02-13 12:21:07 -0600

    ORM - Mapping - Named Queries

--- Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -3,8 +3,10 @@ package com.cybertek.repository;
 import com.cybertek.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -25,4 +27,12 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //multiple bind parameter
     @Query("SELECT e FROM Employee e WHERE e.email=?1 AND e.salary=?2")
     Optional<Employee> getEmployeeByEmailAndSalary(String email, Integer salary);
+
+    //single bind parameter
+    @Query("SELECT e FROM Employee e WHERE e.salary=:salary")
+    Employee getEmployeeBySalary(@Param("salary") int salary);
+
+    //multiple bind parameter
+    @Query("SELECT e FROM Employee e WHERE e.firstName=:name OR e.salary=:salary")
+    List<Employee> getEmployeeByByFirstNameOrSalary(@Param("name") String name, @Param("salary") int salary);
 }
