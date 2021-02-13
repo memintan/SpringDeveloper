4d3be94b3ea9052ac59378df7decf209b15ff4c1
memintan <memintan@gmail.com>
2021-02-13 12:04:03 -0600

    ORM - Mapping - Named Queries

--- Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -2,6 +2,7 @@ package com.cybertek.repository;
 
 import com.cybertek.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -10,33 +11,9 @@ import java.util.List;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
-    //Display all employees with email address ''
-    List<Employee> findByEmail (String email);
+    @Query("SELECT e FROM Employee e WHERE e.email='dtrail8@tamu.edu'")
+    Employee getEmployeeDetail();
 
-    //Display all employees with first name '' and last name '', also show all employees with a email address ''
-    List<Employee> findByFirstNameAndLastNameOrEmail(String firstName, String lastName, String email);
-
-    //Display all employees that first name is not ''
-    List<Employee> findByFirstNameIsNot(String firstName);
-
-    //Display all employees where last name starts with ''
-    List<Employee> findByLastNameStartsWith (String pattern);
-
-    //Display all employees with salaries higher than ''
-    List<Employee> findBySalaryGreaterThan(Integer salary);
-
-    //Display all employees with salaries less than ''
-    List<Employee> findBySalaryLessThan(Integer salary);
-
-    //Display all employees that has been hired between '' and ''
-    List<Employee> findByHireDateBetween(LocalDate startDate, LocalDate endDate);
-
-    //Display all employees where salaries greater and equal to '' in order
-    List<Employee> findBySalaryIsGreaterThanEqualOrderBySalaryDesc (Integer salary);
-
-    //Display top unique 3 employees that is making less than ''
-    List<Employee> findDistinctTop3BySalaryLessThan (Integer salary);
-
-    //Display all employees that do not have email address
-    List<Employee> findByEmailIsNull();
+    @Query("SELECT e.salary FROM Employee e WHERE e.email='dtrail8@tamu.edu'")
+    Integer getEmployeeSalary();
 }
