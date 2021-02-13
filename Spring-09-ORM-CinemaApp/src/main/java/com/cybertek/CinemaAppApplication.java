f0ad18b591f524359a82e5ffa9682540ecad295b
memintan <memintan@gmail.com>
2021-02-13 15:27:24 -0600

    ORM - Mapping - All Queries

--- Spring-09-ORM-CinemaApp/src/main/java/com/cybertek/CinemaAppApplication.java
+++ Spring-09-ORM-CinemaApp/src/main/java/com/cybertek/CinemaAppApplication.java
@@ -1,13 +1,41 @@
 package com.cybertek;
 
+import com.cybertek.repository.AccountRepository;
+import com.cybertek.repository.CinemaRepository;
+import com.cybertek.repository.MovieCinemaRepository;
+import com.cybertek.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class CinemaAppApplication {
+	@Autowired
+	AccountRepository accountRepository;
+	@Autowired
+	CinemaRepository cinemaRepository;
+	@Autowired
+	MovieCinemaRepository movieCinemaRepository;
+	@Autowired
+	TicketRepository ticketRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaAppApplication.class, args);
 	}
+
+	@PostConstruct
+	public void testAccount(){
+		System.out.println("accountRepository.fetchAdminUsers()-------------------------------------------------------------------> " + accountRepository.fetchAdminUsers());
+		System.out.println("cinemaRepository.distinctBYSponsoredName()------------------------------------------------------------> " + cinemaRepository.distinctBYSponsoredName());
+		System.out.println("movieCinemaRepository.countAllByCinemaId(4L)----------------------------------------------------------> " + movieCinemaRepository.countAllByCinemaId(4L));
+		System.out.println("movieCinemaRepository.retrieveAllByLocationName(\"AMC Empire 25\")------------------------------------> " + movieCinemaRepository.retrieveAllByLocationName("AMC Empire 25"));
+		System.out.println("ticketRepository.fetchAllTicketsByUserJPQL(4l)--------------------------------------------------------> " + ticketRepository.fetchAllTicketsByUserJPQL(4l));
+		System.out.println("ticketRepository.fetchAllTicketsWithRangeDates(LocalDateTime.now().minusDays(25),LocalDateTime.now())-> " + ticketRepository.fetchAllTicketsWithRangeDates(LocalDateTime.now().minusDays(25), LocalDateTime.now()));
+		System.out.println("ticketRepository.retrieveAllBySearchCriteria(\"it\")--------------------------------------------------> " + ticketRepository.retrieveAllBySearchCriteria("it"));
+	}
+
 
 }
