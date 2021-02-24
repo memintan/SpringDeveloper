76e3e4c3b93e13bde039bec5c8bbb5907e37cef1
memintan <memintan@gmail.com>
2021-02-24 15:44:45 -0600

    Spring REST - HttpVerbs Legacy

--- Spring-11-Rest-HttpVerbs-Legacy/src/main/java/com/cybertek/implementation/ProductServiceImpl.java
+++ Spring-11-Rest-HttpVerbs-Legacy/src/main/java/com/cybertek/implementation/ProductServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class ProductServiceImpl  implements ProductService {
+public class ProductServiceImpl implements ProductService {
 
     private ProductRepository productRepository;
 
