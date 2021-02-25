0c065c732e50de8c6181dad35e144037674b4a97
memintan <memintan@gmail.com>
2021-02-25 01:23:45 -0600

    Spring REST - Response Entity

--- Spring-11-Rest-ResponceEntity/src/main/java/com/cybertek/controller/ProductController.java
+++ Spring-11-Rest-ResponceEntity/src/main/java/com/cybertek/controller/ProductController.java
@@ -3,7 +3,10 @@ package com.cybertek.controller;
 import com.cybertek.entity.Product;
 import com.cybertek.service.ProductService;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -38,18 +41,39 @@ public class ProductController {
     }
 
     @PostMapping
-    public List<Product> createProduct(@RequestBody Product product){
-        return productService.createProduct(product);
+    public ResponseEntity<List<Product>> createProduct(@RequestBody Product product){
+
+        List<Product> set = productService.createProduct(product);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Version", "Cybertek.V1")
+                .header("Operation", "Create")
+                .body(set);
     }
 
     @DeleteMapping(value = "/{id}")
-    public List<Product> deleteProduct(@PathVariable("id") long id){
-        return productService.delete(id);
+    public ResponseEntity<List<Product>> deleteProduct(@PathVariable("id") long id){
+
+        HttpHeaders responseHttpHeaders = new HttpHeaders();
+        responseHttpHeaders.set("Version","Cybertek.v1");
+        responseHttpHeaders.set("Operation","Delete");
+
+        List<Product> list = productService.delete(id);
+
+        return new ResponseEntity<>(list,responseHttpHeaders,HttpStatus.OK);
     }
 
     @PutMapping(value = "/{id}")
-    public List<Product> updateProduct(@PathVariable("id") long id, @RequestBody Product product){
-        return productService.updateProduct(id, product);
+    public ResponseEntity<List<Product>> updateProduct(@PathVariable("id") long id, @RequestBody Product product){
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("Version", "Cybertek.V1");
+        map.add("Operation", "Update");
+
+        List<Product> list = productService.updateProduct(id, product);
+
+        return new ResponseEntity<>(list,map,HttpStatus.OK);
     }
 
 
