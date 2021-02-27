10735c2d8c29ee5be85271d4c8d2cb7c350b1407
memintan <memintan@gmail.com>
2021-02-27 10:25:42 -0600

    Spring REST - School Lab

--- Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/model/Teacher.java
+++ Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/model/Teacher.java
@@ -1,4 +1,47 @@
 package com.cybertek.model;
 
-public class Teacher {
+import com.cybertek.enums.EducationLevel;
+import com.cybertek.enums.Status;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "teacher")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer"}, ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Teacher extends BaseEntity{
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    private LocalDate birthday;
+
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "education_level")
+    private EducationLevel educationLevel;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    private Address address;
 }
