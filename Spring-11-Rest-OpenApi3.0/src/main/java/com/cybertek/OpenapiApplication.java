9b94b852925e3fc02893576cd415f7d369820ea3
memintan <memintan@gmail.com>
2021-03-01 01:03:38 -0600

    Spring REST - Open API 3.0

--- Spring-11-Rest-OpenApi3.0/src/main/java/com/cybertek/OpenapiApplication.java
+++ Spring-11-Rest-OpenApi3.0/src/main/java/com/cybertek/OpenapiApplication.java
@@ -1,13 +1,26 @@
 package com.cybertek;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.criteria.CriteriaBuilder;
 
 @SpringBootApplication
 public class OpenapiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OpenapiApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI()
+                .info(new Info().title("Cinema Application")
+                .version("v1")
+                .description("Selling online cinema tickets"));
     }
 
 }
