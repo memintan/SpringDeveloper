fc4a516db514c189a1cd39d4babb16be5a9d03e2
memintan <memintan@gmail.com>
2021-03-01 23:51:33 -0600

    Spring REST - Security JWT

--- Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/service/UserService.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/service/UserService.java
@@ -5,19 +5,23 @@ import com.cybertek.enums.UserState;
 import com.cybertek.repository.UserRepository;
 import org.hibernate.service.spi.ServiceException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 
 import javax.transaction.Transactional;
 import java.util.List;
 
+@Service
 public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
+
     public User readByUsername(String username) {
         return userRepository.findByUsername(username).orElse(null);
     }
@@ -27,8 +31,10 @@ public class UserService {
     public List<User> getAll() {
         return userRepository.findAll();
     }
+
     @Transactional
     public User createUser(User user) throws ServiceException {
+
         User foundUserByEmail = readByEmail(user.getEmail());
         User foundUserByUsername = readByUsername(user.getUsername());
         if(foundUserByEmail != null) {
@@ -37,16 +43,19 @@ public class UserService {
         if(foundUserByUsername != null) {
             throw new ServiceException("This user already exists, please change your username");
         }
+
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         user.setIsVerified(false);
         return userRepository.save(user);
     }
+
     @Transactional
     public User verifyUser(User user) {
         user.setIsVerified(true);
         user.setState(UserState.ACTIVE);
         return userRepository.save(user);
     }
+
     @Transactional
     public void deleteUser(Integer id) throws ServiceException {
         User user = userRepository.findById(id).orElse(null);
@@ -56,8 +65,10 @@ public class UserService {
         user.setIsVerified(false);
         userRepository.save(user);
     }
+
     @Transactional
     public User resetPassword(User user) throws ServiceException {
+
         User foundUser = userRepository.findByEmail(user.getEmail()).orElse(null);
         if (foundUser == null) {
             throw new ServiceException("User with email does not exists: " + user.getEmail());
