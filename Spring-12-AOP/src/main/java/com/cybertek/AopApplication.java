bd372cb21c24f8455784fbda00fa7f3d09754d60
memintan <memintan@gmail.com>
2021-03-08 14:46:29 -0600

    Spring AOP

--- Spring-12-AOP/src/main/java/com/cybertek/AopApplication.java
+++ Spring-12-AOP/src/main/java/com/cybertek/AopApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AopApplication {
+public class AopApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(AopApplication.class, args);
