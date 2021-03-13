fe76132659f13b482161eba51341c5d1c8de55f9
memintan <memintan@gmail.com>
2021-03-13 01:42:36 -0600

    MockMvc

--- Spring-13-UnitTesting-MockMvc/src/test/java/com/cybertek/UnitTestingImplTest.java
+++ Spring-13-UnitTesting-MockMvc/src/test/java/com/cybertek/UnitTestingImplTest.java
@@ -2,16 +2,21 @@ package com.cybertek;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UnitTestingImplTest {
 
     @Mock
     DataRepository dataRepository;
+
+    @InjectMocks
+    UnitTestingImpl unitTesting;
 
     @Test
     void calculateSum() {
@@ -27,6 +32,16 @@ class UnitTestingImplTest {
         DataRepositoryImpl dataRepository = new DataRepositoryImpl();
         UnitTestingImpl unitTesting = new UnitTestingImpl(dataRepository);
         int actual = unitTesting.calculateSumUsingDataService();
+        assertEquals(6,actual);
+    }
+
+    @Test
+    void calculateSumUsingDataService_Mock(){
+
+        when(dataRepository.findAll()).thenReturn(new int[]{1,2,3});
+
+        int actual = unitTesting.calculateSumUsingDataService();
+
         assertEquals(6,actual);
     }
 }
