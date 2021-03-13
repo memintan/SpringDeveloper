f3230199bff43711f803ba621e9fd102643d626a
memintan <memintan@gmail.com>
2021-03-13 01:37:31 -0600

    MockMvc

--- Spring-13-UnitTesting-MockMvc/src/main/java/com/cybertek/UnitTestingImpl.java
+++ Spring-13-UnitTesting-MockMvc/src/main/java/com/cybertek/UnitTestingImpl.java
@@ -4,8 +4,22 @@ import java.util.Arrays;
 
 public class UnitTestingImpl {
 
+    DataRepository dataRepository;
+
+    public UnitTestingImpl(DataRepository dataRepository) {
+        this.dataRepository = dataRepository;
+    }
+
+    public UnitTestingImpl() {
+    }
+
     public int calculateSum(int[] data){
         int sum = 0;
         return Arrays.stream(data).sum();
+    }
+
+    public int calculateSumUsingDataService(){
+        int sum=0;
+        return Arrays.stream(dataRepository.findAll()).sum();
     }
 }
--- Spring-13-UnitTesting-MockMvc/src/test/java/com/cybertek/UnitTestingImplTest.java
+++ Spring-13-UnitTesting-MockMvc/src/test/java/com/cybertek/UnitTestingImplTest.java
@@ -1,10 +1,17 @@
 package com.cybertek;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class UnitTestingImplTest {
+
+    @Mock
+    DataRepository dataRepository;
 
     @Test
     void calculateSum() {
@@ -12,5 +19,14 @@ class UnitTestingImplTest {
         UnitTestingImpl unitTesting = new UnitTestingImpl();
         int actual = unitTesting.calculateSum(new int[]{1,2,3});
         assertEquals(6, actual);
+    }
+
+    @Test
+    void calculateSumUsingDataService(){
+
+        DataRepositoryImpl dataRepository = new DataRepositoryImpl();
+        UnitTestingImpl unitTesting = new UnitTestingImpl(dataRepository);
+        int actual = unitTesting.calculateSumUsingDataService();
+        assertEquals(6,actual);
     }
 }
